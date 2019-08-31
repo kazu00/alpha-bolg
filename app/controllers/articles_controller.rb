9cba7180bd62625a80664bdcd3c367ececdbdbf4
kazu00 <c6h8o6112233@yahoo.co.jp>
2019-08-31 12:34:45 +0000

    記事の編集、更新、詳細とインデックスを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Articles.all
+  end
+
   def new
     @article = Article.new
+  end
+  
+  def edit
+    # edit.htmlから情報を受け取る？
+    @article = Article.find(params[:id ])
+    if @article.update(article_params)
+      flash[:notice] = "記事の編集に成功しました"
+      # 受け取った@articleをshow.htmlで表示する
+      redirect_to article_path(@article)
+    else
+      # edit.htmファイルを表示する
+      render 'edit'
+    end
   end
   
   def create    
